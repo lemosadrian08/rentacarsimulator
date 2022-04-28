e5c3e64e065d354384f0dfc68722eb34b3af8585
lemosadrian08 <lemosadrian08@gmail.com>
2022-04-28 20:47:47 -0300

    add HOF

--- main.js
+++ main.js
@@ -1,4 +1,14 @@
 let saldo = 100000
+
+const meses=["enero","febrero","marzo","abril","mayo","junio","julio","agosto","septiembre","octubre","noviembre","diciembre"]
+const mesDisponibleCronos=["abril","mayo","junio"]
+const mesDisponibleOnix=["mayo","junio","julio"]
+const mesDisponibleRaptor=["junio","julio","agosto"]
+
+function buscarMes(arr,mes){
+    return arr.some(a=>a===mes )
+}
+
 class Autos{
     constructor(marca, modelo, precio) {
         this.marca=marca
@@ -9,7 +19,6 @@ class Autos{
 const cronos=new Autos ("fiat", "cronos", 5000)
 const onix=new Autos ("chevrolet", "onix", 6000)
 const raptor=new Autos("ford", "raptor", 10000)
- 
 
 alert("Bienvenido a Rent a Card")
 alert("Lo primero que debe hacer es crear un usuario y contraseña")
@@ -21,12 +30,11 @@ let contraseñaN = prompt("Cree una contraseña")
 usuarioContraseña.push(contraseñaN)
 
 
-
 ingresarUsuario()
 function ingresarUsuario(){
     let usuario = prompt("Ingrese su usario")
     if (usuario ===usuarioContraseña[0]){
-        alert ("Bienbenido")
+        alert ("Bienbenido "+usuario)
         ingreseClave()
     }else{
         alert("Usuario incorrecto")
@@ -45,22 +53,63 @@ function ingreseClave(){
     }
 } 
  
-
-
 function seleccionAuto(){
     let op = prompt("Seleccione una opcion: \n 1. Fiat Cronos 1.8 Drive $5000/dia \n 2. Chevrolet Onix 2022 $6000/dia \n 3. Ford Raptor F-150 $10000/dia")
     switch(op){
         case "1":
-            diaEntrega(cronos.marca, cronos.modelo, cronos.precio)
+            mesCronos()
             break
         case "2":
-            diaEntrega(onix.marca, onix.modelo, onix.precio)
+            mesOnix()
             break
         case "3":
-            diaEntrega(raptor.marca, raptor.modelo, raptor.precio)
+            mesRaptor()
             break
         default:
             break
+    }
+}
+
+function mesCronos (){
+    let mes = prompt("Ingrese el mes en el que desea alquilar el auto")
+    if (buscarMes(mesDisponibleCronos,mes)){
+        alert("El auto esta disponible ese mes")
+        diaEntrega(cronos.marca, cronos.modelo, cronos.precio)
+    } else if(buscarMes(meses,mes)){
+        alert("El auto no esta disponible en ese mes, por favor elija otro mes")
+        mesCronos()
+    } else{
+        alert("Error, ingrese un mes")
+        mesCronos()
+    }
+}
+
+function mesOnix (){
+    let mes = prompt("Ingrese el mes en el que desea alquilar el auto")
+    if (buscarMes(mesDisponibleOnix,mes)){
+        alert("El auto esta disponible ese mes")
+        diaEntrega(onix.marca, onix.modelo, onix.precio)
+    } else if(buscarMes(meses,mes)){
+        alert("El auto no esta disponible en ese mes, por favor elija otro mes")
+        mesOnix()
+    } else{
+        alert("Error, ingrese un mes")
+        mesOnix()
+    }
+}
+
+
+function mesRaptor (){
+    let mes = prompt("Ingrese el mes en el que desea alquilar el auto")
+    if (buscarMes(mesDisponibleRaptor,mes)){
+        alert("La camioneta esta disponible ese mes")
+        diaEntrega(raptor.marca, raptor.modelo, raptor.precio)
+    } else if(buscarMes(meses,mes)){
+        alert("La camioneta no esta disponible en ese mes, por favor elija otro mes")
+        mesRaptor()
+    } else{
+        alert("Error, ingrese un mes")
+        mesRaptor()
     }
 }
 
