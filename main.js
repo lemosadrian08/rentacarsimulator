01072d50e3095d7b035e28db7dc1bd346e8e5a4a
lemosadrian08 <lemosadrian08@gmail.com>
2022-05-09 22:44:58 -0300

    add Events

--- main.js
+++ main.js
@@ -1,204 +1,165 @@
-let saldo = 100000
 
-const meses=["enero","febrero","marzo","abril","mayo","junio","julio","agosto","septiembre","octubre","noviembre","diciembre"]
-const mesDisponibleCronos=["abril","mayo","junio"]
-const mesDisponibleOnix=["mayo","junio","julio"]
-const mesDisponibleRaptor=["junio","julio","agosto"]
+const autos=[]
+const autoSeleccionado=[]
+const dias=[]
+const diaSeleccionadoEntrega=[]
+const diaSeleccionadoDevolucion=[]
+let saldo = 40000
 
-function buscarMes(arr,mes){
-    return arr.some(a=>a===mes )
-}
-
-class Autos{
-    constructor(marca, modelo, precio) {
+class Auto{
+    constructor(marca, modelo, precio, img) {
+        this.id=autos.length
         this.marca=marca
         this.modelo=modelo
         this.precio=precio
+        this.img=img
     }
 }
-const cronos=new Autos ("Fiat", "Cronos", 5000)
-const onix=new Autos ("Chevrolet", "Onix", 6000)
-const raptor=new Autos("Ford", "Raptor", 10000)
+autos.push(new Auto ("Fiat", "Cronos", 5000, "../Simulador/img/cronos.jpg"))
+autos.push(new Auto ("Chevrolet", "Onix", 6000, "../Simulador/img/onix.jpg"))
+autos.push(new Auto("Ford", "Raptor", 10000, "../Simulador/img/raptor.jpg")) 
 
-alert("Bienvenido a Rent a Card")
-alert("Lo primero que debe hacer es crear un usuario y contraseña")
-    
-const usuarioContraseña =[]
-let usuarioN = prompt("Cree un usuario")
-usuarioContraseña.push(usuarioN)
-let contraseñaN = prompt("Cree una contraseña")
-usuarioContraseña.push(contraseñaN)
+class Dia{
+    constructor(dia) {
+        this.id=dias.length
+        this.dia=dia
+    }
+}
+
+dias.push(new Dia ("Lunes"))
+dias.push(new Dia ("Martes"))
+dias.push(new Dia ("Miercoles"))
+dias.push(new Dia ("Jueves"))
+dias.push(new Dia ("Viernes"))
+dias.push(new Dia ("Sabado"))
+dias.push(new Dia ("Domingo"))
+
+mostrarAutos(autos)
+
+function mostrarAutos(autos){
+    const preguntaAuto=document.createElement("h3")
+    preguntaAuto.innerHTML=`Seleccione el vehiculo que desea rentar`
+    document.body.appendChild(preguntaAuto)
+    for (const auto of autos){
+        let card = document.createElement("div")
+        card.innerHTML+= `<div class="card" style="width: 18rem;">
+      <img src="${auto.img}" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">${auto.marca} ${auto.modelo}</h5>
+        <p class="card-text">$${auto.precio}/dia</p>
+      </div>
+    </div>`;
+    for (const auto of autos){
+        document.body.appendChild(card)}
+        
+        let btnRent = document.createElement('button');
+        btnRent.textContent=`Rentar`
+        document.body.appendChild(btnRent)
+        btnRent.onclick= ()=>{
+            rentarAuto(auto.id)
+            mostrarDias() 
+        }
+    }
+}
+
+function rentarAuto(id){
+    autoSeleccionado.pop()
+    const seleccion = autos.find(auto => auto.id == id)
+    autoSeleccionado.push(seleccion)
+
+}
+
+function mostrarDias(){
+    const preguntaEntrega=document.createElement("h3")
+    preguntaEntrega.innerHTML=`Seleccione el dia de entrega del auto`
+    document.body.appendChild(preguntaEntrega)
+
+    for (const dia of dias){
+        const btnseleccionDia = document.createElement("div")
+        btnseleccionDia.innerHTML=`<button>${dia.dia}</button>`
+        document.body.appendChild(btnseleccionDia)
+        btnseleccionDia.onclick= ()=>{
+            rentarDia(dia.id)
+    }
 
 
-ingresarUsuario()
-function ingresarUsuario(){
-    let usuario = prompt("Ingrese su usario")
-    if (usuario ===usuarioContraseña[0]){
+    }
 
-        let usuarioHTML = document.getElementById("bienvenida");
-        usuarioHTML.innerHTML = `Bienbenido ${usuario}`;
-        ingreseClave()
+}
+
+function rentarDia(id){
+    diaSeleccionadoEntrega.pop()
+    const seleccion = dias.find(dia=> dia.id ==id)
+    diaSeleccionadoEntrega.push(seleccion)
+    notificacion(autoSeleccionado,diaSeleccionadoEntrega)
+}
+
+function notificacion (autoSeleccionado,diaSeleccionadoEntrega){
+    const mensajeNotificacion = document.createElement("h2")
+    mensajeNotificacion.innerHTML= `Le entregaremos el ${autoSeleccionado[0].marca} ${autoSeleccionado[0].modelo} el dia ${diaSeleccionadoEntrega[0].dia}`
+    document.body.appendChild(mensajeNotificacion)
+    mostrarDiaDevolucion()
+}
+
+function mostrarDiaDevolucion (){
+    const preguntaDevolucion=document.createElement("h3")
+    preguntaDevolucion.innerHTML=`Seleccione el dia que usted devolvera el auto`
+    document.body.appendChild(preguntaDevolucion)
+    for (const dia of dias){
+        const btnseleccionDia = document.createElement("div")
+        btnseleccionDia.innerHTML=`<button>${dia.dia}</button>`
+        document.body.appendChild(btnseleccionDia)
+        btnseleccionDia.onclick= ()=>{
+        diaDevolucion(dia.id)
+        }
+    }
+
+}
+function diaDevolucion(id){
+    diaSeleccionadoDevolucion.pop()
+    const seleccion = dias.find(dia=> dia.id ==id)
+    diaSeleccionadoDevolucion.push(seleccion)
+    notificacionDevolucion(autoSeleccionado,diaSeleccionadoDevolucion)
+}
+
+function notificacionDevolucion (autoSeleccionado,diaSeleccionadoDevolucion){
+    const mensajeNotificacion = document.createElement("h2")
+    mensajeNotificacion.innerHTML= `Usted debera devolver el ${autoSeleccionado[0].marca} ${autoSeleccionado[0].modelo} el dia ${diaSeleccionadoDevolucion[0].dia}`
+    document.body.appendChild(mensajeNotificacion)
+    total(autoSeleccionado,diaSeleccionadoEntrega,diaSeleccionadoDevolucion)
+}
+
+function total (autoSeleccionado,diaSeleccionadoEntrega,diaSeleccionadoDevolucion){
+    const totalDias = (diaSeleccionadoDevolucion[0].id-diaSeleccionadoEntrega[0].id+1)
+    const totalPrecio= totalDias*autoSeleccionado[0].precio
+    const resumen=document.createElement("h3")
+    resumen.innerHTML=`El costo por alqular el ${autoSeleccionado[0].marca} ${autoSeleccionado[0].modelo} por ${totalDias} dias es de $${totalPrecio} <br>Usted dispone de un saldo de $${saldo}<br>¿Desea abonarlo?`
+    document.body.appendChild(resumen)
+    preguntaPago(totalPrecio)
+}
+
+function preguntaPago(totalPrecio){
+
+    const btnPagar =document.createElement("button")
+    btnPagar.innerHTML=`Pagar`
+    document.body.appendChild(btnPagar)
+    btnPagar.onclick=()=>{
+        pagar(totalPrecio)
+    }
+}
+
+function pagar(totalPrecio){
+    console.log(totalPrecio)
+    if (totalPrecio<=saldo){
+        saldo -=totalPrecio
+        const saludo =document.createElement("h3")
+        saludo.innerHTML=`El pago se realizo con exito, gracias por confiar en nosotros!<br>Le entregaremos el vehiculo el dia ${diaSeleccionadoEntrega[0].dia}<br>Su saldo ahora es de $${saldo}`
+        document.body.appendChild(saludo)
+
+    }else if(totalPrecio>saldo){
+        alert("saldo insuficiente")
     }else{
-        alert("Usuario incorrecto")
-        ingresarUsuario()
+        alert("error")
     }
+
 }
-
-function ingreseClave(){
-    let clave = prompt("Ingrese su clave")
-    if (clave ===usuarioContraseña[1]){
-        alert("Clave correcta")
-        seleccionAuto()
-    }else{
-        alert("Clave incorrecta")
-        ingreseClave()
-    }
-} 
-
-function seleccionAuto(){
-    let op = prompt("Seleccione una opcion: \n 1. Fiat Cronos 1.8 Drive $5000/dia \n 2. Chevrolet Onix 2022 $6000/dia \n 3. Ford Raptor F-150 $10000/dia")
-    switch(op){
-        case "1":
-            let imgCHTML = document.getElementById("img");
-            imgCHTML.innerHTML = `<img src="./img/cronos.jpg">`;
-            mesCronos()
-            break
-        case "2":
-            let imgOHTML = document.getElementById("img");
-            imgOHTML.innerHTML = `<img src="./img/onix.jpg">`;
-            mesOnix()
-            break
-        case "3":
-            let imgRHTML = document.getElementById("img");
-            imgRHTML.innerHTML = `<img src="./img/raptor.jpg">`;
-            mesRaptor()
-            break
-        default:
-            break
-    }
-}
-
-
-
-
-function mesCronos (){
-    let mes = prompt("Ingrese el mes en el que desea alquilar el auto")
-    if (buscarMes(mesDisponibleCronos,mes)){
-        alert("El auto esta disponible ese mes")
-        diaEntrega(cronos.marca, cronos.modelo, cronos.precio,mes)
-    } else if(buscarMes(meses,mes)){
-        alert("El auto no esta disponible en ese mes, por favor elija otro mes")
-        mesCronos()
-    } else{
-        alert("Error, ingrese un mes")
-        mesCronos()
-    }
-}
-
-function mesOnix (){
-    let mes = prompt("Ingrese el mes en el que desea alquilar el auto")
-    if (buscarMes(mesDisponibleOnix,mes)){
-        alert("El auto esta disponible ese mes")
-        diaEntrega(onix.marca, onix.modelo, onix.precio,mes)
-    } else if(buscarMes(meses,mes)){
-        alert("El auto no esta disponible en ese mes, por favor elija otro mes")
-        mesOnix()
-    } else{
-        alert("Error, ingrese un mes")
-        mesOnix()
-    }
-}
-
-function mesRaptor (){
-    let mes = prompt("Ingrese el mes en el que desea alquilar el auto")
-    if (buscarMes(mesDisponibleRaptor,mes)){
-        alert("La camioneta esta disponible ese mes")
-        diaEntrega(raptor.marca, raptor.modelo, raptor.precio,mes)
-    } else if(buscarMes(meses,mes)){
-        alert("La camioneta no esta disponible en ese mes, por favor elija otro mes")
-        mesRaptor()
-    } else{
-        alert("Error, ingrese un mes")
-        mesRaptor()
-    }
-}
-
-function diaEntrega(marca,modelo,precio,mes){
-    let diaE = prompt("Ingrese la fecha de entrega del auto")
-    if((diaE>=1)&&(diaE<31)){
-        diaDevolucion(marca,modelo,precio,mes,diaE)
-    }else {
-        alert("Error")
-        diaEntrega(marca,modelo,precio,mes)
-    }
-}
-
-function diaDevolucion(marca,modelo,precio,mes,diaE){
-    let diaD = prompt("Ingrese la fecha de devolucion del auto")
-    if((diaD>=1)&&(diaD<31)&&(diaE<diaD)){
-        precioT(marca,modelo,precio,mes,diaE,diaD)
-    }else {
-        alert("Error")
-        diaEntrega(marca,modelo,precio,mes)
-    }
-}
-
-function precioT(marca,modelo,precio,mes,diaE,diaD){
-    
-    let diaT = diaD-diaE
-    let total = diaT*precio
-    let resumenHTML = document.getElementById("costo");
-    resumenHTML.innerHTML = `El costo por rentar el ${marca} ${modelo} por ${diaT} dias, es de: $${total}`;
-    document.body.append(resumenHTML);
-    tarjeta(marca,modelo,precio,mes,diaE,diaD,total)
-}
- 
-function tarjeta(marca,modelo,precio,mes,diaE,diaD,total){
-    confirm("Ingrese su tarjeta")
-    if (saldo>total){
-        let resumenHTML = document.createElement("h2");
-        resumenHTML.innerHTML = `El pago se realizo con exito <br>Le entregaremos el ${marca} ${modelo} el dia ${diaE} de ${mes}<br>Gracias por confiar en nosotros`;
-        document.body.append(resumenHTML);
-    } else {
-        alert("Saldo insuficiente")
-        diaEntrega(marca,modelo,precio,mes)
-    }
-}
-
-
-
-
-
-
-
-/* const disponibilidadOnix = [
-    {dia:"lunes", disponible:true},
-    {dia:"martes", disponible:true},
-    {dia:"miercoles", disponible:true},
-    {dia:"jueves", disponible:true},
-    {dia:"viernes", disponible:true},
-    {dia:"sabado", disponible:true},
-    {dia:"domingo", disponible:true}
-] 
-let diaB =prompt("DIA")
-let buscar = disponibilidadOnix.find((el)=>el.dia ===diaB)
-
-if(buscar.disponible==true){
-    buscar.disponible=false
-}else{
-    alert("no")
-}
-console.log(disponibilidadOnix)
-
-
-
-let diaB2 =prompt("DIA2")
-let buscar2 = disponibilidadOnix.find((el)=>el.dia ===diaB2)
-
-if(buscar2.disponible==true){
-    buscar2.disponible=false
-}else {
-    alert("no disponible ese dia")
-}
-
-console.log(disponibilidadOnix)  */
